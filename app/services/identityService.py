2120cb8516d6e1c0d4a2220b25db497067418470
sudotracker <f20160711@hyderabad.bits-pilani.ac.in>
2021-07-09 01:17:10 +0530

    otpResponse

--- app/services/identityService.py
+++ app/services/identityService.py
@@ -33,7 +33,7 @@ def send_otp_request(request: OtpCreateRequest, db: Session):
     db.add(save_otp)
     db.commit()
     db.refresh(save_otp)
-    return "Otp sent to {request.email}."
+    return {"otpResponse":"Otp sent to {request.email}."}
 
 
 def register_user(request: UserCreateRequest, db: Session):
