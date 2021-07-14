91622cabcf624d45e6555354f4016f2aa8167342
sudotracker <f20160711@hyderabad.bits-pilani.ac.in>
2021-07-14 23:50:30 +0530

    deletedUserApiDeveloped

--- app/routers/userProfileDetailsRouter.py
+++ app/routers/userProfileDetailsRouter.py
@@ -18,7 +18,7 @@ router = APIRouter(
 )
 
 
-@router.get('/get',response_model=UserProfileResponse)
+@router.get('/get',response_model=UserProfileResponse,status_code=status.HTTP_200_OK)
 def get_user_profile_details(db: Session = Depends(database.get_db), user_id=Depends(auth_handler.auth_wrapper)):
     return userProfileDetailsService.get_profile_details(user_id,db)
 
@@ -26,3 +26,7 @@ def get_user_profile_details(db: Session = Depends(database.get_db), user_id=Dep
 @router.put('/update', status_code=status.HTTP_200_OK)
 def update_user_profile_details(request: UserProfileUpdateRequest, db: Session = Depends(get_db), user_id=Depends(auth_handler.auth_wrapper)):
     return userProfileDetailsService.update_profile_details(request,user_id, db)
+
+@router.delete('/delete', status_code=status.HTTP_200_OK)
+def delete_user_profile_details(delete_string: str, db: Session = Depends(get_db), user_id=Depends(auth_handler.auth_wrapper)):
+    return userProfileDetailsService.delete_user_profile_details(delete_string,user_id, db)
